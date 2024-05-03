4c3896dbf371f5cdf9d7880aade92c1f771c3f29
edgarptz <phuthinh12252002@gmail.com>
2024-05-03 15:15:12 +0700

    update

---
+++ main.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, jsonify
+from g_drive_service import GoogleDriveService
+
+app=Flask(__name__)
+service=GoogleDriveService().build()
+
+from io import BytesIO
+from googleapiclient.http import MediaIoBaseUpload
+from datetime import datetime
+
+@app.get('/upload')
+def index():
+    return "Hello, World!"
+
+@app.post('/upload')
+def upload_file():
+    
+    uploaded_file=request.files.get("file")
+
+    buffer_memory=BytesIO()
+    uploaded_file.save(buffer_memory)
+
+    media_body=MediaIoBaseUpload(uploaded_file, uploaded_file.mimetype, resumable=True)
+    created_at= datetime.now().strftime("%Y%m%d%H%M%S")
+    file_metadata={
+        "name":f"{uploaded_file.filename} ({created_at})",
+        "appProperties":{
+            "file_description":request.form.get("description")
+        }
+    }
+
+    returned_fields="id, name, mimeType, webViewLink, exportLinks, appProperties"
+    
+    upload_response=service.files().create(
+        body = file_metadata, 
+        media_body=media_body,  
+        fields=returned_fields
+    ).execute()
+
+    return upload_response
+
+ 
+if __name__=='__main__':
+    app.run(debug=True, port=8000)
