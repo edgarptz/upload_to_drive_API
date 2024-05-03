3a35000238103d862e6e4f593ec5ba6f21e21ea2
edgarptz <phuthinh12252002@gmail.com>
2024-05-03 14:03:53 +0700

    update app

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 from flask import Flask, request, jsonify
-from google.oauth2.credentials import Credentials
+import os
+from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 
 app = Flask(__name__)
 
-CLIENT_SECRET_FILE = 'client_secret.json'
-SCOPES = ['https://www.googleapis.com/auth/drive.file']
-creds = Credentials.from_authorized_user_file(CLIENT_SECRET_FILE, SCOPES)
+SCOPES = ['https://www.googleapis.com/auth/drive']
+SERVICE_ACCOUNT_FILE = "service_account.json"
 
-@app.route('/upload', methods=['POST'])
+credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+@app.route('/', methods=['POST'])
 def upload_files():
     uploaded_files = request.files.getlist('file')  # Get a list of all the uploaded files
     if not uploaded_files:
         return jsonify({'error': 'No files provided'}), 400
 
-    service = build('drive', 'v3', credentials=creds)
+    service = build('drive', 'v3', credentials=credentials)
     upload_responses = []
 
     for file in uploaded_files:
